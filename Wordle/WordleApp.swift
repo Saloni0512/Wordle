976c77c4de2f7cf4bfc403cca8bdfb8496ffb36c
Saloni0512 <sinceresaloni@gmail.com>
2024-11-27 21:06:12 +0530

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Saloni Agarwal on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordleTests/WordleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordleTests.swift
+//  WordleTests
+//
+//  Created by Saloni Agarwal on 11/27/24.
+//
+
+import Testing
+@testable import Wordle
+
+struct WordleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
